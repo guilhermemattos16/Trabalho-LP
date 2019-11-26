6bc7f1a46357837fe748d36c9c4476fe77e92f48
Guilherme Mattos <45467253+guilhermemattos16@users.noreply.github.com>
2019-11-26 14:06:13 -0300

    merge Luciano

---
+++ jagpdf-1.4.0.win32.x86.c_cpp/include/jagpdf/api.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2005-2009 Jaroslav Gresula
+ *
+ * Distributed under the MIT license (See accompanying file
+ * LICENSE.txt or copy at http://jagpdf.org/LICENSE.txt)
+ *
+ */
+#ifndef API_JG232_H__
+#define API_JG232_H__
+
+#include "C:\Users\keici\Documents\ufes\LP\trabalhoLP\jagpdf-1.4.0.win32.x86.c_cpp\include\jagpdf\detail\capi.h"
+#include "C:\Users\keici\Documents\ufes\LP\trabalhoLP\jagpdf-1.4.0.win32.x86.c_cpp\include\jagpdf\detail\errcodes.h"
+#include "C:\Users\keici\Documents\ufes\LP\trabalhoLP\jagpdf-1.4.0.win32.x86.c_cpp\include\jagpdf\detail\version.h"
+
+#ifdef __cplusplus
+# include "C:\Users\keici\Documents\ufes\LP\trabalhoLP\jagpdf-1.4.0.win32.x86.c_cpp\include\jagpdf\detail\cppapi.h"
+#endif
+
+#endif
+/** EOF @file */
